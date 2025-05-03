e08edab422d0f5c519c3186fb4e00f8c5c45d39b
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2025-05-03 08:15:22 +0700

    Routing

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,38 @@
-import { Hono } from 'hono'
+import {Hono} from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app
+    .get('/hello/:name', (c) => {
+        const name = c.req.param('name')
+        return c.text(`Hello ${name}`)
+    })
+    .post('/post', (c) => {
+        return c.text("Post Hello")
+    })
 
+    .get('/products/:id{[0-9]+}', async (c) => {
+        const id = c.req.param("id")
+        return c.text(`Product ${id}`)
+    })
+
+    .get('/', (c) => {
+        return c.text('Hello Hono!')
+    })
+
+const book = new Hono().basePath('/api');
+book
+    .get('/book', (c) => {
+        return c.text("Ini halaman Book")
+    })
+
+    .get('/book/a', (c) => {
+        return c.text("Ini halaman Book A")
+    })
+
+    .get('/book/:id', (c) => {
+        return c.text("Ini halaman Book ID")
+    })
+
+app.route('/', book);
 export default app
